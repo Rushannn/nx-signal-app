fa229de66bd787cf29f65797846ff167bc72d2a6
Rushannn <rushankarimov@gmail.com>
2024-03-08 12:23:09 +0300

    feat: implement api.sevice and api-url.token

--- apps/nx-signal-app/src/app/app.config.ts
+++ apps/nx-signal-app/src/app/app.config.ts
@@ -2,7 +2,8 @@ import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { provideHttpClient } from '@angular/common/http';
-// import { environment } from '../environments/environment.development';
+import { API_URL } from '@core/http-client';
+import { environment } from '../environments/environment.development';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -18,6 +19,6 @@ export const appConfig: ApplicationConfig = {
       },
     ]),
     provideHttpClient(), provideAnimationsAsync(),
-    // { provide: API_URL, useValue: environment.api_url },
+    { provide: API_URL, useValue: environment.api_url },
   ],
 };
